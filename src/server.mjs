af04b39ab4993fed5bafba5ade9fea2acbd90fa1
JeffWScott <jeffscott@outlook.com>
2021-11-21 10:18:42 -0500

    increase to 50mb

--- src/server.mjs
+++ src/server.mjs
@@ -8,7 +8,8 @@ import { startRouter } from './router.mjs'
 export const createServer = (port, db) => {
     const app = express();
     app.use(cors())
-    app.use(express.json())
+    app.use(express.json({limit: '50mb', extended: true}));
+    app.use(express.urlencoded({limit: '50mb', extended: true}));
 
     startRouter(app, db)
 
